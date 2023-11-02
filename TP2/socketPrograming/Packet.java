54a9bf989f91200f30939994f25794d437607f81
mirelois <79458822+mirelois@users.noreply.github.com>
2023-11-02 09:59:32 +0000

    bitwise operations lezz goooooooo

--- TP2/socketPrograming/Packet.java
+++ TP2/socketPrograming/Packet.java
@@ -53,8 +53,8 @@ public class Packet {
             this.payload[i - HEADER_SIZE] = packet[i];
         }
 
-        this.sequence_number = packet[1] + (packet[0] << 8);
-        this.time_stamp = packet[5] + (packet[4] << 8) + (packet[3] << 16) + (packet[2] << 24);
+        this.sequence_number = packet[1] | (packet[0] << 8);
+        this.time_stamp = packet[5] | (packet[4] << 8) | (packet[3] << 16) | (packet[2] << 24);
     }
 
     public byte[] getPayload() {
