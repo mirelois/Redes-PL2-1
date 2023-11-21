ed00f94caaa4b1f07a552580945982c65bef0a01
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-11-21 19:14:06 +0000

    Sup agora sabe de quem vem

--- TP2/socketPrograming/Sup.java
+++ TP2/socketPrograming/Sup.java
@@ -5,23 +5,27 @@ public class Sup extends Packet {
 
     static int HEADER_SIZE = 12;
 
+    
     int time_stamp; //2
     int video_time_stamp; // 4 in ms
     int sequence_number; //4
     // int latency; //2
     int checksum; //2
+    int streamId; //1
 
-    public Sup(int video_time_stamp, int sequence_number, InetAddress address, int port, int payload_size, byte[] payload) {
+    public Sup(int streamId, int video_time_stamp, int sequence_number, InetAddress address, int port, int payload_size, byte[] payload) {
 
         super(HEADER_SIZE, payload, payload_size, address, port);
 
         this.sequence_number = sequence_number;
-
+        
         this.video_time_stamp = video_time_stamp;
-
+        
         this.time_stamp = Packet.getCurrTime();
-
+        
         this.checksum = 0; //TODO make good checksum, not bad checksum, only good checksum
+        
+        this.streamId = streamId;
 
         // System.err.println(this.time_stamp);
 
@@ -41,6 +45,7 @@ public class Sup extends Packet {
         this.header[10] = (byte) (this.checksum >> 8   /* & 0xFF */);
         this.header[11] = (byte) (this.checksum        /* & 0xFF */);
 
+        this.header[12] = (byte) (this.streamId        /* & 0xFF */);
     }
 
     public Sup(DatagramPacket packet) throws java.net.UnknownHostException{
@@ -63,6 +68,8 @@ public class Sup extends Packet {
         this.checksum = (Byte.toUnsignedInt(this.header[10]) << 8)  |
                          Byte.toUnsignedInt(this.header[11]);
 
+        this.streamId = Byte.toUnsignedInt(this.header[12]);
+
     }
 
 	public int getTime_stamp() {
@@ -78,6 +85,10 @@ public class Sup extends Packet {
     }
 
 	public int getChecksum() {
+		return checksum;
+	}
+
+    public int getStreamId() {
 		return checksum;
 	}
 }
