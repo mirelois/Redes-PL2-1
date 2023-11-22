eecd6fbd476314440090a63c52ad9b889ba1e528
mirelois <79458822+mirelois@users.noreply.github.com>
2023-11-22 20:29:24 +0000

    battle

---
+++ TP2/socketPrograming/ServerInfo.java
@@ -0,0 +1,10 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class ServerInfo {
+
+    public ArrayList<InetAddress> servers = new ArrayList<>();
+    // public HashMap<InetAddress, ServerInfo> serverMap;
+}
+
