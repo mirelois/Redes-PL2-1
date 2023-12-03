67a375c5805bd1a9afa58526b82d74260b842c2d
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-12-03 23:04:40 +0000

    Update Packet.java

--- TP2/socketPrograming/Protocols/Packet.java
+++ TP2/socketPrograming/Protocols/Packet.java
@@ -47,7 +47,7 @@ public class Packet {
         if(packet.getLength() < header_size) {
             throw new PacketSizeException("Packet size to smol" + "\nIt came from " + this.getAddress() + 
                                           "\nConsidered as " + this.getClass().getName() + 
-                                          ": Header size: " + this.getHeaderSize() + "\nPayload size: " + this.getPayloadSize() + 
+                                          ": Header size: " + header_size + 
                                           "\nPacket size: " + packet.getLength() + 
                                           "\nString: " + new String(packet.getData()));
         }
